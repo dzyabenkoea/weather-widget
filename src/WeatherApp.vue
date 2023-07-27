62b71a3c2d1da28364bfd7d7b18fae4d8a611046
Дзябенко Егор <e.dzyabenko@makeroi.ru>
2023-07-27 19:46:26 +0500

    Настройка проекта

---
+++ src/WeatherApp.vue
@@ -0,0 +1,10 @@
+<template>
+  <article>
+    <cog6-tooth-icon class="h-8"/>
+  </article>
+</template>
+
+<script setup lang="ts">
+import {Cog6ToothIcon} from "@heroicons/vue/24/outline";
+
+</script>
